fea6b23097021df9230f5a4d29236dc92d14c1e7
Anny85-code <aniekaudo@yahoo.com>
2022-09-11 13:41:12 -0700

    Add file no 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,14 +7,14 @@
 int main(void)
 {
 unsigned int i;
-for (i =48; i < 58; i++)
+for (i = 48; i < 58; i++)
 {
 putchar(i);
-}
 if (i != 57)
 {
 putchar(',');
 putchar(' ');
+}
 }
 putchar('\n');
 return (0);

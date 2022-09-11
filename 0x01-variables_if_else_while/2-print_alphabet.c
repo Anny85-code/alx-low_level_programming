476623b3d10c5baf278a61303783d5f4d8dfa84b
Anny85-code <aniekaudo@yahoo.com>
2022-09-11 05:50:29 -0700

    Update file no .2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,7 +7,7 @@
 int main(void)
 {
 char alp;
-alp[26] = {"abcdefghijklmnopqrstuvwxyz"};
+alp[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
 int i;
 for (i = 0; i < 26; i++)
 {

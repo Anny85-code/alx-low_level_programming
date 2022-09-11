1a51d91c7d2c5b4d3459b9b481431253bafde3e5
Anny85-code <aniekaudo@yahoo.com>
2022-09-11 04:48:01 -0700

    Add file 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#includes<stdio.h>
+#includes<stdlib.h>
+/**
+ * main - A function with a print method
+ * Description - Prints a char in uppercase
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+char = alp[26] = "abcdefghijklmnopqrstuvwxyz";
+int i;
+for (i = 0; i < 26; i++)
+{
+putchar(alp[i].upper);
+}
+putchar("\n");
+return (0);
+}

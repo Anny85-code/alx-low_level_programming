02e58d74de4c47d5d70bbe0e6a1b31563f328a27
Anny85-code <aniekaudo@yahoo.com>
2022-09-12 16:14:58 -0700

    Update file no 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -22,11 +22,16 @@ e = 48;
 while (e < 58)
 {
 i = 48;
-while (i < 58)									{										op1 = (h * 10) + g;
-op2 = (e * 10) + i;								if (op1 < op2)
+while (i < 58)
+{
+op1 = (h * 10) + g;
+op2 = (e * 10) + i;
+if (op1 < op2)
 {
 putchar(h);
-putchar(g);									putchar(' ');									putchar(e);
+putchar(g);
+putchar(' ');
+putchar(e);
 putchar(i);
 if (h == 57 && g == 56 && e == 57 && i == 57)
 break;

35eb87a17716d306c1529e0e44c58a98e8afb9a6
Anny85-code <aniekaudo@yahoo.com>
2022-09-15 19:14:55 -0700

    Add main.h file and file no 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Test function to see if interger is positive or negative
+ * Return : Always (0) success
+ */
+
+int main(void)
+{
+int i:
+i = 0;
+positive_or_negative(i);
+return (0);
+}

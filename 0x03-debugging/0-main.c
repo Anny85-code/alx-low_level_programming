4824dac5548bc02cede6bd2823ff7194a7d1d475
Anny85-code <aniekaudo@yahoo.com>
2022-09-15 20:22:55 -0700

    Update main file

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,7 +3,7 @@
 /**
  * main - Test function to see if interger is positive or negative
  * Description - Test for positive and negative
- * Return : Always (0) success
+ * Return: Always (0) success
  */
 
 int main(void)

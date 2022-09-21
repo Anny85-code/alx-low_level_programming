704599070bca847c4790b6143a8bb476153a5d28
Anny85-code <aniekaudo@yahoo.com>
2022-09-21 03:57:56 -0700

    Add file no 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - Concatenate strings
+ * @dest: holds a string
+ * @src: holds a string
+ * Descriprition - concatenate strings
+ * Return: Always 0 success
+ */
+char *_strcat(char *dest, char *src)
+{
+*strcat(*dest, *src);
+}

283fd6f6fa86d3a81e82e58c34f2902de8056ad9
Anny85-code <aniekaudo@yahoo.com>
2022-09-22 01:20:47 -0700

    Update file no 1

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,5 +10,18 @@
  */
 char *_strcat(char *dest, char *src)
 {
-*strcat(*dest, *src);
+int count = 0, count2 = 0;
+while (*(dest + count) != '\0')
+{
+count++;
+}
+while (count2 >= 0)
+{
+*(dest + count) = *(src + count2);
+if (*(src + count2) == '\0')
+break;
+count++;
+count2++;
+}
+return (dest);
 }

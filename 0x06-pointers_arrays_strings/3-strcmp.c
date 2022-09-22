0b5f384a43d6c776ebbfc89d2c5407fbfb1f3050
Anny85-code <aniekaudo@yahoo.com>
2022-09-22 03:35:01 -0700

    Add file 4

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strcmp - Compares strings
+ * Description - Compares strings
+ * @s1: holds a string
+ * @s2: holds a string
+ * Return: Always 0 success
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+int count;
+int count2;
+while (*(s1 + count) == '\0')
+{
+count++;
+}
+while (*(s2 + count2) == '\0')
+{
+if (*(s1 + count) == *(s2 + count2))
+	break;
+int op = *(s1) - *(s2);
+count2++;
+}
+return (op);
+}

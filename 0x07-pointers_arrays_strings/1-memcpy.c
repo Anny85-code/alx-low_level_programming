fc408a0ffcc02a371dfb8c9eb0904cb7f4a0b757
Anny85-code <aniekaudo@yahoo.com>
2022-09-28 05:46:59 -0700

    Add file 2

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _memcpy - Copy addres form src to dest
+* @dest: Holds array address
+* @src: Holds array poiinter values
+* @n: Holds integer
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+for (i = 0; i < n; i++)
+{
+*(src + i) = dest;
+}
+return (dest);
+}

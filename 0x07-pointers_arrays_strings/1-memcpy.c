e0d0936d0c6ad3f69605fcfb0cc721713b2f21a0
Anny85-code <aniekaudo@yahoo.com>
2022-09-28 05:48:30 -0700

    Update file 2

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
 * _memcpy - Copy addres form src to dest
+* Description - Copy address
 * @dest: Holds array address
 * @src: Holds array poiinter values
 * @n: Holds integer

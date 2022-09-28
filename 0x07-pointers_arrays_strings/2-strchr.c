9ccc65043747d36d8e2d1582c15ba433fc64a2cd
Anny85-code <aniekaudo@yahoo.com>
2022-09-28 06:16:28 -0700

    Add file no 3

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* _strchr - Locates a char
+* Description - The function locates a char
+* @s: holds a string pointer
+* @c: holds a char
+* Return: A string
+*/
+char *_strchr(char *s, char c)
+{
+unsigned int i;
+for (i = 0; i < (*s = '\0'); i++)
+{
+if (*(s + i) == c)
+{
+break;
+return (c);
+}
+else
+{
+return (null);
+}
